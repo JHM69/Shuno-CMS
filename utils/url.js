bfee0d3aaf04d5d9058bcc1b2ea4812b1803ec62
JHM69 <jahangirhmarup@gmail.com>
2024-02-25 17:55:02 +0600

    Update API base URL

--- utils/url.js
+++ utils/url.js
@@ -3,7 +3,7 @@ const url = process.env.API;
 
 export const getBaseUrl = () => {
     // console.log(process.env.API);
-    return process.env.API || 'http://localhost:5000/api';
+    return process.env.API || 'http://54.80.47.120:5000/api';
 }
 
 //'http://54.80.47.120:5000/api';
